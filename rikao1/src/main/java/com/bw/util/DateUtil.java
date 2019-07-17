1edd2010cbab33e7584825e90c55f7319e042d44
秦 <2197999639@qq.com>
2019-07-17 09:12:02 +0800

    提交了时间工具类

---
+++ rikao1/src/main/java/com/bw/util/DateUtil.java
@@ -0,0 +1,64 @@
+package com.bw.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtil {
+	
+	
+	//方法1：根据传入的日期获取年龄
+	
+	public static int getAge (Date src) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+		Calendar calendar = sdf.getCalendar();
+		Date time = calendar.getTime();
+		long time2 = src.getTime();
+		
+		return 0;
+	
+	}
+	
+	//方法2：根据传入的参数获取该日期的月初日期，
+	//例如给定的日期为“2019-09-19 19:29:39”，返回“2019-09-01 00:00:00”
+	public static Date getDateByMonthInit (Date src) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+		
+		
+		return src;
+	
+	}
+	
+	//方法3 :根据传入的参数获取该日器的月末日期，
+	//例如给定的日期为“2019-09-19 19:29:39”，返回“2019-09-30 23:59:59”，
+	//注意月大月小以及闰年。
+	public static Date getDateByMonthLast(Date src) {
+	
+		
+		
+		return src;
+		
+	}
+//	方法4：求未来日期离今天还剩的天数
+	public static int getDaysByFuture (Date future) {
+		
+		
+		
+		
+		return 0;
+		
+	}
+	
+	//方法5：求过去日期离今天过去的天数
+	public static int getDaysByDeparted (Date departed) {
+		
+		
+		
+		return 0;
+		
+	}
+
+
+}
---
+++ rikao1/src/test/java/com/bw/test/TestDate.java
@@ -0,0 +1,36 @@
+package com.bw.test;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.bw.util.DateUtil;
+
+public class TestDate {
+	
+	@Test
+	public void Test1(){
+		
+		DateUtil.getAge(new Date(2000-11-20));
+	}
+	@Test
+	public void Test2(){
+		
+		DateUtil.getAge(new Date(2000-11-20));
+	}
+	@Test
+	public void Test3(){
+		
+		DateUtil.getAge(new Date(2000-11-20));
+	}
+	@Test
+	public void Test4(){
+		
+		DateUtil.getAge(new Date(2000-11-20));
+	}
+	@Test
+	public void Test5(){
+		
+		DateUtil.getAge(new Date(2000-11-20));
+	}
+}
